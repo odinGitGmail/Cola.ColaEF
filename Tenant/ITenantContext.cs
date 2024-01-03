1201418c950a45bb71b0064e347645d1d4ccf550
Din Ya <>
2024-01-03 16:54:38 +0800

    init cola.ef

---
+++ Tenant/ITenantContext.cs
@@ -0,0 +1,17 @@
+﻿using Cola.ColaEF.Models;
+using SqlSugar;
+
+namespace Cola.ColaEF.Tenant;
+
+/// <summary>
+///     租户上下文
+/// </summary>
+public interface ITenantContext
+{
+    int? TenantId { get; }
+
+    void SetDbClientByTenant(List<AopOnLogExecutingModel>? aopOnLogExecutingModels,
+        List<AopOnErrorModel>? aopOnErrorModels, List<GlobalQueryFilter>? globalQueryFilters);
+    
+    SqlSugarClient GetDbClientByTenant(int? tenantId);
+}
